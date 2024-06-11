69bf023741c144160f447ab91874b734306a8692
Gal-Blech <120471305+Gal-Blech@users.noreply.github.com>
2024-06-11 14:29:58 +0300

    Update sections_test.py

--- sections_test.py
+++ sections_test.py
@@ -37,7 +37,7 @@ def create_dropdowns(options, level=0, path=[], set_index=0):
                 path = path + [choice]
                 st.write(f"Column's attribute will be: **{'.'.join(path)}**")
                 st.session_state[f'path_{set_index}'] = path
-    st.write(f"Column's attribute will be: **{'.'.join(path)}**")
+    st.write(f"Column's attribute is: **{path}**")
 
 # Initialize dropdowns
 if 'dropdown_set_count' not in st.session_state:
